c53b9f119b3aed8a3c4385db29bb1b6662e37561
田爪大智 <>
2024-08-10 18:33:13 +0900

    created canvas function

--- src/components/HandWritten.tsx
+++ src/components/HandWritten.tsx
@@ -1,26 +1,71 @@
 /** @jsxRuntime classic */
 /** @jsx jsx */
 
-import React, { FC, memo } from "react";
+import React, { FC, memo, useRef, useState } from "react";
 import { jsx, css } from "@emotion/react";
-import styled from "@emotion/styled";
 import { Button, Flex, Heading } from "@chakra-ui/react";
 
 const HandWritten: FC = memo(() => {
-  const StyledCanvas = styled.canvas`
-    width: 28px;
-    height: 28px;
-    border: 1px solid black;
-    margin: 6px 0;
-  `;
+  const canvasRef = useRef<HTMLCanvasElement | null>(null);
+  const [isDrawing, setIsDrawing] = useState<boolean>(false);
+
+  const canvasSize = {
+    width: 28,
+    height: 28,
+  };
+
+  const startDrawing = (x: number, y: number) => {
+    setIsDrawing(true);
+    const ctx = canvasRef.current?.getContext("2d");
+    ctx?.beginPath();
+    ctx?.moveTo(x, y);
+  };
+
+  const endDrawing = () => {
+    setIsDrawing(false);
+  };
+
+  const draw = (x: number, y: number) => {
+    if (!isDrawing) return;
+    const canvas = canvasRef.current;
+    const ctx = canvas?.getContext("2d");
+    ctx?.lineTo(x, y);
+    ctx?.stroke();
+  };
+
+  const clearDrawing = () => {
+    const ctx = canvasRef.current?.getContext("2d");
+    ctx?.clearRect(0, 0, canvasSize.width, canvasSize.height);
+  };
+
   return (
     <Flex direction="column" alignItems="center">
       <Heading as="h1" mb={5}>
         Hand Written Recognition
       </Heading>
-      <StyledCanvas />
+      <canvas
+        css={css`
+          border: 1px solid black;
+        `}
+        width={canvasSize.width}
+        height={canvasSize.height}
+        ref={canvasRef}
+        onMouseDown={(e: React.MouseEvent) => {
+          startDrawing(e.nativeEvent.offsetX, e.nativeEvent.offsetY);
+        }}
+        onMouseUp={endDrawing}
+        onMouseLeave={endDrawing}
+        onMouseMove={(e: React.MouseEvent) =>
+          draw(e.nativeEvent.offsetX, e.nativeEvent.offsetY)
+        }
+      />
       <Flex direction="row">
-        <Button mr={5} colorScheme="teal" variant="outline">
+        <Button
+          mr={5}
+          colorScheme="teal"
+          variant="outline"
+          onClick={clearDrawing}
+        >
           reset
         </Button>
         <Button colorScheme="teal" variant="solid">
