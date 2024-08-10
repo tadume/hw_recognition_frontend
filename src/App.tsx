fe8868c73d1a1f39cdf8755b8384dc438437e12e
田爪大智 <>
2024-08-10 11:35:31 +0900

    create canvas page

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import "./App.css";
 import { BrowserRouter } from "react-router-dom";
 import Router from "./router/Router";
+import { ChakraProvider } from "@chakra-ui/react";
+import theme from "./theme";
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Router />
-      </BrowserRouter>
+      <ChakraProvider theme={theme}>
+        <BrowserRouter>
+          <Router />
+        </BrowserRouter>
+      </ChakraProvider>
     </div>
   );
 }
--- src/components/HandWritten.tsx
+++ src/components/HandWritten.tsx
@@ -1,7 +1,34 @@
+/** @jsxRuntime classic */
+/** @jsx jsx */
+
 import React, { FC, memo } from "react";
+import { jsx, css } from "@emotion/react";
+import styled from "@emotion/styled";
+import { Button, Flex, Heading } from "@chakra-ui/react";
 
 const HandWritten: FC = memo(() => {
-  return <div>HelloWorld</div>;
+  const StyledCanvas = styled.canvas`
+    width: 28px;
+    height: 28px;
+    border: 1px solid black;
+    margin: 6px 0;
+  `;
+  return (
+    <Flex direction="column" alignItems="center">
+      <Heading as="h1" mb={5}>
+        Hand Written Recognition
+      </Heading>
+      <StyledCanvas />
+      <Flex direction="row">
+        <Button mr={5} colorScheme="teal" variant="outline">
+          reset
+        </Button>
+        <Button colorScheme="teal" variant="solid">
+          predict
+        </Button>
+      </Flex>
+    </Flex>
+  );
 });
 
 export default HandWritten;
