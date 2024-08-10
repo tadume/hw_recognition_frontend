c2cd2c6a1b73f6efeb4fde141e159cf7b197359c
田爪大智 <>
2024-08-10 18:57:57 +0900

    created predict function

--- src/components/HandWritten.tsx
+++ src/components/HandWritten.tsx
@@ -1,9 +1,10 @@
 /** @jsxRuntime classic */
 /** @jsx jsx */
 
-import React, { FC, memo, useRef, useState } from "react";
-import { jsx, css } from "@emotion/react";
-import { Button, Flex, Heading } from "@chakra-ui/react";
+import React, { FC, memo, useRef, useState } from 'react';
+import { jsx, css } from '@emotion/react';
+import { Button, Flex, Heading } from '@chakra-ui/react';
+import axios from 'axios';
 
 const HandWritten: FC = memo(() => {
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
@@ -16,7 +17,7 @@ const HandWritten: FC = memo(() => {
 
   const startDrawing = (x: number, y: number) => {
     setIsDrawing(true);
-    const ctx = canvasRef.current?.getContext("2d");
+    const ctx = canvasRef.current?.getContext('2d');
     ctx?.beginPath();
     ctx?.moveTo(x, y);
   };
@@ -28,14 +29,28 @@ const HandWritten: FC = memo(() => {
   const draw = (x: number, y: number) => {
     if (!isDrawing) return;
     const canvas = canvasRef.current;
-    const ctx = canvas?.getContext("2d");
+    const ctx = canvas?.getContext('2d');
     ctx?.lineTo(x, y);
     ctx?.stroke();
   };
 
   const clearDrawing = () => {
-    const ctx = canvasRef.current?.getContext("2d");
+    const ctx = canvasRef.current?.getContext('2d');
     ctx?.clearRect(0, 0, canvasSize.width, canvasSize.height);
+  };
+
+  const predict = async () => {
+    const base64String = canvasRef.current?.toDataURL('image/png');
+    const res = await axios
+      .post('http://localhost:8000/api/predict', {
+        image: base64String,
+      })
+      .catch((e) => {
+        console.log(e);
+        alert('an error occured while predicting');
+      });
+
+    console.log(res);
   };
 
   return (
@@ -68,7 +83,7 @@ const HandWritten: FC = memo(() => {
         >
           reset
         </Button>
-        <Button colorScheme="teal" variant="solid">
+        <Button colorScheme="teal" variant="solid" onClick={predict}>
           predict
         </Button>
       </Flex>
